b1ee5b5bfedf1ca6486f2f29b1b7ef5ab96cac33
Tommy Zhu <tzhu2001@gmail.com>
2021-11-20 13:44:56 -0500

    push to dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,29 @@
-import Ts from "rollup-plugin-typescript2";
+// import Ts from "rollup-plugin-typescript2";
 
-export default {
-  input: ["src/index.ts"],
-  output: {
-    dir: "lib",
-    format: "esm",
-    sourcemap: true,
+// export default {
+//   input: ["src/index.ts"],
+//   output: {
+//     dir: "lib",
+//     format: "esm",
+//     sourcemap: true,
+//   },
+//   plugins: [Ts()],
+//   external: ["react"],
+// };
+
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' },
+    ],
+    plugins: [
+      typescript(),
+    ],
+    external: Object.keys(pkg.peerDependencies || {})
   },
-  plugins: [Ts()],
-  external: ["react"],
-};
+];
