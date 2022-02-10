75b176a8851043bf5eecac1cc305351525b3a5f3
Tommy Zhu <tzhu2001@gmail.com>
2022-02-10 18:07:50 -0500

    export in the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import AnnotateCanvas from './component/core/AnnotateCanvas';
+import { MediaAnnotationContextProvider } from './component/core/AnnotateCanvas/MediaAnnotateContext';
+import { PlayerContextProvider } from './component/core/MediaPlayer/PlayerContext';
+
+import PanZoom from './component/core/PanZoom';
+import {
+  PanZoomContent,
+  PanZoomOverlay,
+} from './component/core/PanZoom/PanZoom';
+
+import * as playerComposer from './component/composer/playerComposer';
+import * as annotateComposer from './component/composer/annotateComposer';
+
 
 import {
   AnnotateElement,
@@ -10,5 +22,14 @@ export {
   AnnotateElement,
   AnnotateElementType,
   AnnotateCanvas,
+  PanZoom,
+  PanZoomContent,
+  PanZoomOverlay,
   PlayDeck,
+  
+  PlayerContextProvider,
+  MediaAnnotationContextProvider,
+  
+  playerComposer,
+  annotateComposer,
 };
